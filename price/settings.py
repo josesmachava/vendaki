25da0fa227fda749b8247f9305da823f3dda11b2
= <jose.s.machava@gmail.com>
2019-09-28 23:50:13 +0200

    add staticfiles

--- price/settings.py
+++ price/settings.py
@@ -124,3 +124,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'price', 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

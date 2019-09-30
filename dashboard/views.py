5789830ea95c213eadd3c833715a15d4195cff84
jeremias <you@example.com>
2019-09-30 19:19:14 +0200

    dashboard template

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
 
 # Create your views here.
 
 
 
-def index(self, request):
-    return render(request, "index.html")
+def index( request):
+    return render(request, "dashboard/index.html")

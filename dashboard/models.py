e5e03d00e8e70cb0cec3d1a8c4fbc01e2d61fafe
= <jose.s.machava@gmail.com>
2019-10-21 12:17:15 +0200

    add products

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from account.models import  User
 
 # Create your models here.
 class SocialMedia(models.Model):
     name = models.CharField(max_length=30, blank=True)
     url = models.URLField()
+
 
 class Categories(models.Model):
     name = models.CharField(max_length=30, blank=True)
@@ -18,3 +20,15 @@ class Product(models.Model):
     discount = models.CharField(max_length=30, blank=True)
 
 
+class OrderProduct():
+    item = models.ForeignKey(Product, on_delete=models.CASCADE())
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User)
+    product = models.ManyToManyField(OrderProduct)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.first_name

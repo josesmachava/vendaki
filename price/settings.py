2e1b3cddfd2a9f7ac623367a1dea47d0b7e1423e
= <jose.s.machava@gmail.com>
2019-11-12 01:16:02 +0200

    add everthing

--- price/settings.py
+++ price/settings.py
@@ -79,6 +79,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "django.contrib.auth.middleware.AuthenticationMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     "django.contrib.auth.middleware.AuthenticationMiddleware",
 
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -154,12 +156,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'price', 'static'),
 )
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 EMAIL_HOST = 'mail.preco.co.mz'
 EMAIL_PORT = 26
@@ -168,7 +174,4 @@ EMAIL_HOST_PASSWORD = '849394995Jose'
 EMAIL_USE_TLS = True
 
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'

6b0cb5e5859669d9344a3af82bfd23bbba92a091
= <jose.s.machava@gmail.com>
2019-12-23 03:52:38 +0200

    improve menu

--- price/static/javascript/app.js
+++ price/static/javascript/app.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
+            $(".mobile-utilities").hide();
 
-            $(".mobile-utilities").hide()
+     $(".mobile-menu-items").hide()
     $(".show-footer").click(function () {
 
         $(".mobile-utilities").show()
+    })
+     $(".mobile-menu-icon").click(function () {
+       $(".mobile-menu-items").show('slow')
     })
 });
